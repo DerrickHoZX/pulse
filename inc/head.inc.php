43290a8aeb959970dbcd792e4ea50dcf05edb0a2
abi-2901 <2502602@sit.singaporetech.edu.sg>
2026-03-18 18:35:40 +0800

    edited css config in head.inc.php

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -13,6 +13,6 @@
     <!-- Custom JS -->
     <script defer src="js/main.js"></script>
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/main.css?v=2">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
