5894277c266b3047cce1bcd074b3885895a5c734
Au Myat <210099a@student.tp.edu.sg>
2026-03-19 11:12:18 +0800

    Color Changes

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -13,6 +13,6 @@
     <!-- Custom JS -->
     <script defer src="js/main.js"></script>
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/main.css?v=8">
+    <link rel="stylesheet" href="css/main.css?v=9">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
